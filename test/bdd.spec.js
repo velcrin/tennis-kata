d41235cc65d31731c3a4c7a924147861dcc97fdb
Vincent <vincent.elcrin@gmail.com>
2014-04-05 13:45:25 +0200

    Improve bdd lib by adding more context to gwt

--- test/bdd.spec.js
+++ test/bdd.spec.js
@@ -1,33 +1,28 @@
-require('should');
-var khan = require('../lib/bdd.js').khan;
+var chai = require('chai'),
+    khan = require('../lib/bdd.js').khan;
+chai.Should();
 
 
-describe('Game', function () {
+describe('Khan', function () {
 
-    it('when should add a & b to equal c', khan({
-
-        given: {a: 6, b: 4, c: 10},
-
-        when: function (a, b) {
+    it('should inject variable defined in given clause where requested', khan({
+        "Given 3 numbers a, b & c": {a: 6, b: 4, c: 10},
+        "When we add a to b": function (a, b) {
             this.result = a + b;
         },
-
-        then: function (c) {
+        "Then the result should be c": function (c) {
             this.result.should.equal(c);
         }
     }));
 
-    it('when should multiply a by b then equal c for each values given to a, b & c', khan({
-
-        when: function (a, b) {
+    it('should iter on where clause and inject values where requested', khan({
+        "when we multiply a & b": function (a, b) {
             this.result = a * b;
         },
-
-        then: function (c) {
+        "then the result should be c": function (c) {
             this.result.should.equal(c);
         },
-
-        where: [
+        "where a, b & c get the following values": [
             {a: 3, b: 4, c: 12},
             {a: 4, b: 2, c: 8}
         ]
